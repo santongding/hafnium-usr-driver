4569b9e806dd2a833b1a9a3f6aab26e7b2ac2879
santongding <1349880437@qq.com>
2022-10-08 01:57:46 +0800

    backup

--- main.c
+++ main.c
@@ -13,9 +13,9 @@
 #include <linux/platform_device.h>
 #include <linux/proc_fs.h>
 #include <linux/slab.h>
-extern spinlock_t hf_send_lock;
-extern struct page *hf_send_page;
-extern struct page *hf_recv_page;
+spinlock_t hf_send_lock;
+struct page *hf_send_page;
+struct page *hf_recv_page;
 struct mailbox_buffers {
 	void *send;
 	void *recv;
@@ -91,8 +91,8 @@ static void handle_input(void *input, size_t len)
 	pr_info("read %u bit(s)", len);
 	struct hf_usr_protocol *req = input;
 	ffa_vm_id_t callee_id = req->vm_id;
-	struct ffa_value ret =
-		ffa_msg_send_direct_req(current_vm_id, callee_id, 23, 0, 0, 0, 0);
+	struct ffa_value ret = ffa_msg_send_direct_req(current_vm_id, callee_id,
+						       23, 0, 0, 0, 0);
 	if (ret.func == FFA_INTERRUPT_32) {
 		ret = ffa_run_till_not_interrupt(callee_id,
 						 ffa_vcpu_index(ret));
@@ -193,6 +193,7 @@ static struct platform_driver hf_usr_driver = {
 };
 static void share_mem(void)
 {
+	ffa_vm_id_t target_vm = 0x8002;
 	int i;
 	int8_t *not_aligned_shared_pages = kmalloc(PAGE_SIZE * 3, GFP_KERNEL);
 	int8_t *shared_pages =
@@ -224,8 +225,9 @@ static void share_mem(void)
 	spin_lock_irqsave(&hf_send_lock, flags);
 
 	EXPECT_EQ(ffa_memory_region_init_single_receiver(
-			  mb.send, HF_MAILBOX_SIZE, current_vm_id, 2, constituents,
-			  ARRAY_SIZE(constituents), 0, 0, FFA_DATA_ACCESS_RW,
+			  mb.send, HF_MAILBOX_SIZE, current_vm_id, target_vm,
+			  constituents, ARRAY_SIZE(constituents), 0, 0,
+			  FFA_DATA_ACCESS_RW,
 			  FFA_INSTRUCTION_ACCESS_NOT_SPECIFIED,
 			  FFA_MEMORY_NORMAL_MEM, FFA_MEMORY_CACHE_WRITE_BACK,
 			  FFA_MEMORY_INNER_SHAREABLE, &total_length,
@@ -260,7 +262,7 @@ static void share_mem(void)
 	EXPECT_EQ(ret.func, FFA_SUCCESS_32);
 	EXPECT_EQ(ffa_mem_success_handle(ret), handle);
 	pr_info("Got handle %#llx.\n", handle);
-	EXPECT_NE(handle, 0);
+	EXPECT_EQ(handle, 0);
 	EXPECT_NE(handle & FFA_MEMORY_HANDLE_ALLOCATOR_MASK,
 		  FFA_MEMORY_HANDLE_ALLOCATOR_HYPERVISOR);
 
@@ -270,10 +272,20 @@ static void share_mem(void)
 		shared_pages[i] = i;
 	}
 	pr_info("finish share mem to vm: %u", 2);
+
+	ffa_vm_id_t callee_id = target_vm;
+	struct ffa_value ret = ffa_msg_send_direct_req(current_vm_id, callee_id,
+						       handle, 0, 0, 0, 0);
+	/*if (ret.func == FFA_INTERRUPT_32) {
+		ret = ffa_run_till_not_interrupt(callee_id,
+						 ffa_vcpu_index(ret));
+	}*/
+	pr_info("vm %u ret with code: %x, %d", callee_id, ret.func, ret.arg2);
 }
 
 static int hf_usr_driver_init(void)
 {
+	struct ffa_value ffa_ret;
 	int ret;
 
 	pr_info("hf_usr_driver init\n");
@@ -297,10 +309,36 @@ static int hf_usr_driver_init(void)
 		return ret;
 	}
 
+	/* Allocate a page for send and receive buffers. */
+	hf_send_page = alloc_page(GFP_KERNEL);
+	if (!hf_send_page) {
+		pr_err("Unable to allocate send buffer\n");
+		return -ENOMEM;
+	}
+
+	hf_recv_page = alloc_page(GFP_KERNEL);
+	if (!hf_recv_page) {
+		__free_page(hf_send_page);
+		hf_send_page = NULL;
+		pr_err("Unable to allocate receive buffer\n");
+		return -ENOMEM;
+	}
+	ffa_ret = ffa_rxtx_map(page_to_phys(hf_send_page),
+			       page_to_phys(hf_recv_page));
+	if (ffa_ret.func != FFA_SUCCESS_32) {
+		pr_err("Unable to configure VM mailbox.\n");
+		log_ret(ffa_ret);
+		ret = -EIO;
+		__free_page(hf_recv_page);
+		__free_page(hf_send_page);
+		hf_recv_page = NULL;
+		hf_send_page = NULL;
+	}
+
 	// run_vm();
-	// share_mem();
+	share_mem();
 	pr_info("hf_usr_driver inited");
-	return 0;
+	return ret;
 }
 
 static void hf_usr_driver_exit(void)
@@ -308,6 +346,17 @@ static void hf_usr_driver_exit(void)
 	pr_info("hf_usr_driver exit\n");
 	platform_driver_unregister(&hf_usr_driver);
 	proc_remove(hf_proc_file);
+
+	ffa_rx_release();
+	ffa_rxtx_unmap();
+	if (hf_send_page) {
+		__free_page(hf_send_page);
+		hf_send_page = NULL;
+	}
+	if (hf_recv_page) {
+		__free_page(hf_recv_page);
+		hf_recv_page = NULL;
+	}
 }
 
 module_init(hf_usr_driver_init);
